86f842b8d41792ae1b947a8aaf3b8225323a437c
MuhammadAKJ <Muhammadakj11@gmail.com>
2024-01-05 12:13:10 +0100

    test3

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    skills = models.CharField(max_length=255, blank=True)
+    interests = models.CharField(max_length=255, blank=True)
+    portfolio_url = models.URLField(blank=True)
+    contact_email = models.EmailField(blank=True)
+    phone_number = models.PhoneNumberField(blank=True)
+    public_profile = models.BooleanField(default=True)
+    # social media links
+    twitter_url = models.CharField(max_length=255, blank=True)
+    linkedin_url = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('index') 
+        else:
+            messages.info(request, 'Username or Password not correct')
+            return render(request, 'login.html')
+    else:
+
+        return render(request, 'login.html')
+
+
+def signup(request):
+
+    if request.method == 'POST':
+
+
+        username = request.POST['username']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Username is taken! Try something else')
+            return redirect('signup')
+        elif User.objects.filter(email=email).exists():
+            messages.info(
+                request, 'Email exists in the database, try Login or Forgot Password')
+            return redirect('signup')
+        elif password1 != password1:
+            messages.info(request, 'Passwords doesn\'t match')
+            return redirect('signup')
+        else:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=password1, 
+                                            phone=phone)
+            user.save()
+  
+            return redirect('login')
+    else:
+        return render(request, 'signup.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
